2fdd5648e22cab32e98a33776370f1e32ea7266b
clayton <cgblue99@gmail.com>
2026-08-13 17:03:27 -0400

    hopefully fixed stuff

--- Notification.py
+++ Notification.py
@@ -1,37 +1,63 @@
-#imports for text
+# imports for text
 from twilio.rest import Client
 import twilio
 from requests.models import Response
 
-#Email imports
+# Email imports
 import smtplib
 from email.mime.text import MIMEText
-#Others:
 
+# Slack imports
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Others:
 from requests.api import request
 import json
 import requests
-important = open('pass.json')
+
+important = open("pass.json")
 important = json.load(important)
 print("Notification online")
+
+
 def text_me(msg):
     client = Client(important["account_sid"], important["auth_token"])
-    message = client.messages.create(body =  msg, #Message you send
-                        from_ = important["from#"],#Provided phone number
-                        to =    important["to#"])#Your phone number
+    message = client.messages.create(
+        body=msg,  # Message you send
+        from_=important["from#"],  # Provided phone number
+        to=important["to#"],
+    )  # Your phone number
     print("TXT msg sid" + message.sid)
+
 
 def email_me(subject, body):
     msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = important["email_sender"]
-    msg['To'] = ', '.join([important["email_to"]])
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    msg["Subject"] = subject
+    msg["From"] = important["email_sender"]
+    msg["To"] = ", ".join([important["email_to"]])
+    smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     smtp_server.login(important["email_sender"], important["email_app_password"])
-    smtp_server.sendmail(important["email_sender"], [important["email_to"]], msg.as_string())
+    smtp_server.sendmail(
+        important["email_sender"], [important["email_to"]], msg.as_string()
+    )
     smtp_server.quit()
-'''
+
+
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+
+
+def slack_everyone(channel_id, message):
+    try:
+        response = client.chat_postMessage(channel=channel_id, text=message)
+        print(f"Message sent to {channel_id}: {response['message']['text']}")
+    except SlackApiError as e:
+        print(f"Error sending message: {e.response['error']}")
+
+
+"""
 subject = "Email Test Subject"
 body = "This is the body of the text message"
 email_me(subject, body)
-'''
+"""
--- sondeAlert.py
+++ sondeAlert.py
@@ -2,75 +2,137 @@ import sondehub
 import math
 import json
 import Notification
-import maper
+
 import landingGuess
 import time
 import sys
-#import numpy as np
-#Loads in the options file
-options = open('options.json')
+
+# import numpy as np
+# Loads in the options file
+options = open("options.json")
 options = json.load(options)
-#Sets the lat and lon from options
+# Sets the lat and lon from options
 B = options["home_lat"]
 A = options["home_lon"]
 
 print("Sonde Alert online")
-print("Current location: "+str(B)+" ,"+str(A))
-'''
+print("Current location: " + str(B) + " ," + str(A))
+"""
 def calc_angle(a,b,c,d):
     angle =  (((d-b)/(abs(d-b)))*(180*((math.acos((((math.sqrt((d-b)**2)+(c-a)**2))**2+(10**2)-(math.sqrt((d-b)**2+(c-a+10)**2))**2)/(20*((math.sqrt((d-b)**2)+(c-a)**2)))))/math.pi)))
     return angle
 #Idealy this would give you asumuth and elivation at some point
-'''
-#This calculates the distance between 2 sets of cordinites and ruturns it in miles, not sure how accurate this is everware else in the world...
-def calc_distance(a,b,c,d):
-    i = B-d
-    j = A-c
-    distance = math.sqrt(i**2+j**2)
-    distance = distance*54.6
+"""
+
+
+# This calculates the distance between 2 sets of cordinites and ruturns it in miles, not sure how accurate this is everware else in the world...
+def calc_distance(a, b, c, d):
+    i = B - d
+    j = A - c
+    distance = math.sqrt(i**2 + j**2)
+    distance = distance * 54.6
     return distance
 
-#The on msg runs everytime balloon data comes in from sonde hub
+
+# The on msg runs everytime balloon data comes in from sonde hub
 def on_message(message):
-    #C and D are the position lat and lon of the balloon
+    # C and D are the position lat and lon of the balloon
     C = float((message["position"]).split(",")[1])
     D = float((message["position"]).split(",")[0])
-    
-    balloon_alt = message["alt"]#Gets the balloons current alt
-    dist = calc_distance(A,B,C,D)
 
-    if(dist<options["distance"] and int(balloon_alt)<options["note_alt"]):
+    balloon_alt = message["alt"]  # Gets the balloons current alt
+    dist = calc_distance(A, B, C, D)
+
+    if dist < options["distance"] and int(balloon_alt) < options["note_alt"]:
         print(message)
         try:
-            balloon_frequ = ((message["frequency"]))
+            balloon_frequ = message["frequency"]
         except:
             balloon_frequ = "Unknown"
         try:
-            balloon_type = ((message["type"]))
+            balloon_type = message["type"]
         except:
             balloon_type = "Unknown"
         print(round(dist))
         guess_landing = landingGuess.glanding(message)
         print(guess_landing)
-        timeToBalloon = maper.time_to_destination(str(B)+","+str(A), str(guess_landing[0])+","+str(guess_landing[1]))
-        if(options["msg_type"] == "txtmsg"):
-            #for texting the user
-            Notification.text_me("There is a balloon("+balloon_type+") @ "+str(D)+","+str(C)+" At alt:"+str(balloon_alt)+". On:"+str(balloon_frequ)+"MHZ. ETA:"+str(timeToBalloon)+" Predicted landing: "+str(guess_landing[1])+" ,"+str(guess_landing[0]))
-        elif(options["msg_type"] == "email"):
-            #For emailing the user
-            Notification.email_me("Balloon alert","There is a balloon("+balloon_type+") @ "+str(D)+","+str(C)+" At alt:"+str(balloon_alt)+". On:"+str(balloon_frequ)+"MHZ. ETA:"+str(timeToBalloon)+" Predicted landing: "+str(guess_landing[1])+" ,"+str(guess_landing[0]))
+        timeToBalloon = maper.time_to_destination(
+            str(B) + "," + str(A), str(guess_landing[0]) + "," + str(guess_landing[1])
+        )
+        if options["msg_type"] == "txtmsg":
+            # for texting the user
+            Notification.text_me(
+                "There is a balloon("
+                + balloon_type
+                + ") @ "
+                + str(D)
+                + ","
+                + str(C)
+                + " At alt:"
+                + str(balloon_alt)
+                + ". On:"
+                + str(balloon_frequ)
+                + "MHZ. ETA:"
+                + str(timeToBalloon)
+                + " Predicted landing: "
+                + str(guess_landing[1])
+                + " ,"
+                + str(guess_landing[0])
+            )
+        elif options["msg_type"] == "email":
+            # For emailing the user
+            Notification.email_me(
+                "Balloon alert",
+                "There is a balloon("
+                + balloon_type
+                + ") @ "
+                + str(D)
+                + ","
+                + str(C)
+                + " At alt:"
+                + str(balloon_alt)
+                + ". On:"
+                + str(balloon_frequ)
+                + "MHZ. ETA:"
+                + str(timeToBalloon)
+                + " Predicted landing: "
+                + str(guess_landing[1])
+                + " ,"
+                + str(guess_landing[0]),
+            )
         else:
-            #If some setting is wrong it will default to print
-            print("There is a balloon("+balloon_type+") @ "+str(D)+","+str(C)+" At alt:"+str(balloon_alt)+". On:"+str(balloon_frequ)+"MHZ. ETA:"+str(timeToBalloon)+" Predicted landing: "+str(guess_landing[1])+" ,"+str(guess_landing[0]))
+            # If some setting is wrong it will default to print
+            print(
+                "There is a balloon("
+                + balloon_type
+                + ") @ "
+                + str(D)
+                + ","
+                + str(C)
+                + " At alt:"
+                + str(balloon_alt)
+                + ". On:"
+                + str(balloon_frequ)
+                + "MHZ. ETA:"
+                + str(timeToBalloon)
+                + " Predicted landing: "
+                + str(guess_landing[1])
+                + " ,"
+                + str(guess_landing[0])
+            )
         print("Sent msg\nsleeping till next balloon window")
         time.sleep(36000)
-        
+
     else:
         pass
-test = sondehub.Stream(on_message=on_message)
+
+
+# test = sondehub.Stream(on_message=on_message)
+# print(test)
+# on_message(test)
 animation = "|/-\\"
 idx = 0
-while(1):
+while 1:
     print(animation[idx % len(animation)], end="\r")
     idx += 1
     time.sleep(0.2)
